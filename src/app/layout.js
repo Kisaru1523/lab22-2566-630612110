226ca451961e9af30ece08d9fa58af10f58cdbe3
Kisaru1523 <76036259+Kisaru1523@users.noreply.github.com>
2023-10-05 13:48:40 +0700

    finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="630612110"
+                fullName="Wisarut Sada"
                 year="2023"
               />
             </Container>
